42416b558d36b4d4dd4b31f3f93427eaf1963bbd
artem <metisav@gmail.com>
2022-07-16 02:27:29 +0500

    circle fix

--- box.go
+++ box.go
@@ -4,6 +4,10 @@ import (
 	"errors"
 )
 
+const (
+	notInBound = "shape by index doesn't exist or index went out of the range"
+)
+
 // box contains list of shapes and able to perform operations on them
 type box struct {
 	shapes         []Shape
@@ -33,7 +37,7 @@ func (b *box) GetByIndex(i int) (Shape, error) {
 	if b.isIndexExist(i) {
 		return b.shapes[i], nil
 	}
-	return nil, errors.New("shape by index doesn't exist or index went out of the range")
+	return nil, errors.New(notInBound)
 
 }
 
@@ -41,22 +45,22 @@ func (b *box) GetByIndex(i int) (Shape, error) {
 // whether shape by index doesn't exist or index went out of the range, then it returns an error
 func (b *box) ExtractByIndex(i int) (Shape, error) {
 	if b.isIndexExist(i) {
-		extracted_shape := b.shapes[i]
+		extractedShape := b.shapes[i]
 		b.removeElement(i)
-		return extracted_shape, nil
+		return extractedShape, nil
 	}
-	return nil, errors.New("shape by index doesn't exist or index went out of the range")
+	return nil, errors.New(notInBound)
 }
 
 // ReplaceByIndex allows replacing shape by index and returns removed shape.
 // whether shape by index doesn't exist or index went out of the range, then it returns an error
 func (b *box) ReplaceByIndex(i int, shape Shape) (Shape, error) {
 	if b.isIndexExist(i) {
-		extracted_shape := b.shapes[i]
+		extractedShape := b.shapes[i]
 		b.shapes[i] = shape
-		return extracted_shape, nil
+		return extractedShape, nil
 	}
-	return nil, errors.New("shape by index doesn't exist or index went out of the range")
+	return nil, errors.New(notInBound)
 }
 
 // SumPerimeter provides sum perimeter of all shapes in the list.
@@ -80,12 +84,15 @@ func (b *box) SumArea() float64 {
 // RemoveAllCircles removes all circles in the list
 // whether circles are not exist in the list, then returns an error
 func (b *box) RemoveAllCircles() error {
-	var deleted_count int = 0
+	var deletedCount int = 0
 	for k, v := range b.shapes {
 		if v.ShapeType() == "Circle" {
-			b.removeElement(k - deleted_count)
-			deleted_count += 1
+			b.removeElement(k - deletedCount)
+			deletedCount += 1
 		}
+	}
+	if deletedCount == 0 {
+		return errors.New("No circles to delete")
 	}
 	return nil
 }
